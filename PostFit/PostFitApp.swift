806635cac6ba67f2db6f24d8cd64549d016521be
Daipayan <deeptech870@gmail.com>
2025-12-30 13:08:36 -0800

    Initial Commit

---
+++ PostFit/PostFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostFitApp.swift
+//  PostFit
+//
+//  Created by Dip on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PostFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostFitTests/PostFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostFitTests.swift
+//  PostFitTests
+//
+//  Created by Dip on 12/30/25.
+//
+
+import Testing
+@testable import PostFit
+
+struct PostFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
